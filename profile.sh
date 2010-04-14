1d887be33c44b18f1febf6f147d551404b23df31
Ali Clark <emailaliclark@gmail.com>
2010-04-14 01:15:07 +0100

    add mouse support back for nano, use .profile instead of .bash_profile

---
+++ profile.sh
@@ -0,0 +1,34 @@
+# ~/.profile: executed by the command interpreter for login shells.
+# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
+# exists.
+# see /usr/share/doc/bash/examples/startup-files for examples.
+# the files are located in the bash-doc package.
+
+###############################################################################
+
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+#umask 022
+
+###############################################################################
+
+PATH="$PATH:/usr/sbin:/sbin"
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ]; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+export PATH
+
+###############################################################################
+
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -e "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+
+###############################################################################
