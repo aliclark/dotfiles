87da6b4d539e9d054cfd0e483a505f430c34cd39
Ali Clark <ali@clark.gb.net>
2012-09-08 10:48:48 +0100

    only use profile to set the path

--- profile.sh
+++ profile.sh
@@ -12,41 +12,22 @@
 
 ###############################################################################
 
-export SHELL=zsh
-export EDITOR=emacs
-export BROWSER=firefox
+PATH="$PATH"
 
-###############################################################################
-
-if [ -d "/usr/course/agm/gPy" ]; then
-    PYTHONPATH="/usr/course/agm/gPy:$PYTHONPATH"
+if [ -d "/usr/sbin" ]; then
+    PATH="$PATH:/usr/sbin"
 fi
 
-export PYTHONPATH
+if [ -d "/sbin" ]; then
+    PATH="$PATH:/sbin"
+fi
 
-###############################################################################
-
-PATH="$PATH:/usr/sbin:/sbin"
-
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/local/bin" ]; then
-    PATH="$HOME/local/bin:$PATH"
+if [ -d "$HOME/build/android-sdk-linux_x86" ]; then
+    PATH="$PATH:$HOME/build/android-sdk-linux_x86/tools:$HOME/build/android-sdk-linux_x86/platform-tools"
 fi
 
 if [ -d "$HOME/bin" ]; then
     PATH="$HOME/bin:$PATH"
-fi
-
-if [ -d "$HOME/projects/localscripts" ]; then
-    PATH="$HOME/projects/localscripts:$PATH"
-fi
-
-if [ -d "$HOME/projects/utalities/bin" ]; then
-    PATH="$HOME/projects/utalities/bin:$PATH"
-fi
-
-if [ -d "$HOME/build/android-sdk-linux_x86" ]; then
-    PATH="$HOME/build/android-sdk-linux_x86/tools:$HOME/build/android-sdk-linux_x86/platform-tools:$PATH"
 fi
 
 export PATH
@@ -59,12 +40,3 @@ export PATH
 
 ###############################################################################
 
-# if running bash
-#if [ -n "$BASH_VERSION" ]; then
-    # include .bashrc if it exists
-#    if [ -e "$HOME/.bashrc" ]; then
-#        . "$HOME/.bashrc"
-#    fi
-#fi
-
-###############################################################################
